fc41f08500902a9631f14e0e5f7ffb8315b13b93
Walter Holladay <walter@holladays.org>
2011-10-28 21:11:15 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MovieService::Application.config.secret_token = '2a55df509388f33596210d27cebf87ec87b601a770e6fd2476a9876e53c028192865585981eda1ffe7a10e4c85b984be5bf88578baaa809ac8e84a9b3fe0933e'
